4c2b80f6fa8e7d72d4b4f56eb2f39c0b1b0cd1e5
cjdeniz <christiandeniz9@gmail.com>
2026-05-14 13:42:03 -0700

    feat: cloned dealership project and updated menu

---
+++ workshop/src/main/java/com/pluralsight/dealership/Program.java
@@ -0,0 +1,10 @@
+package com.pluralsight.dealership;
+
+import com.pluralsight.dealership.ui.UserInterface;
+
+public class Program {
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        userInterface.display();
+    }
+}
